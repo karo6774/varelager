0678e35c70e60bc2e784f46dcd81daa84f0ff51a
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-05-13 12:31:41 +0200

    Check item name length

--- Varelager/CreateItemPage.cs
+++ Varelager/CreateItemPage.cs
@@ -6,7 +6,7 @@ namespace Varelager
     {
         public string Label => "Create new item";
         private readonly Storage _storage;
-        
+
         public CreateItemPage(Storage storage)
         {
             _storage = storage;
@@ -15,20 +15,22 @@ namespace Varelager
         public void Execute()
         {
             Console.WriteLine("Enter a name for the new item:");
-            var name = Prompt.PromptString();
+            string name;
+            while ((name = Prompt.PromptString()).Length <= 0)
+                Console.WriteLine("Item name must be at least one character long.");
 
             Console.WriteLine();
             Console.WriteLine("Enter amount of the new item:");
             int amount;
-            while ((amount = Prompt.PromptInt()) < 0) 
+            while ((amount = Prompt.PromptInt()) < 0)
                 Console.WriteLine("Amount must be positive.");
 
             var item = new Item(name, amount);
             _storage.addItem(item);
-            
+
             Console.WriteLine($"Created {amount} of item named '{name}'.");
             Console.WriteLine();
-            
+
             Prompt.PromptPageExit();
         }
     }
